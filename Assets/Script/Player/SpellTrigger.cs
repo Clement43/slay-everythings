b7164dc0e82305b3550ff4b2044904c3f9713768
clement <clembernard43@gmail.com>
2025-08-24 22:06:28 +0200

    is pressed pour le trigger spell

--- Assets/Script/Player/SpellTrigger.cs
+++ Assets/Script/Player/SpellTrigger.cs
@@ -1,8 +1,4 @@
-using System;
-using Unity.VisualScripting.Antlr3.Runtime.Misc;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
-using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 public class SpellTrigger : MonoBehaviour
@@ -12,56 +8,59 @@ public class SpellTrigger : MonoBehaviour
     public InputActionReference inputSpell2;
     public InputActionReference inputSpell3;
     public InputActionReference inputSpell4;
+
     private Character character;
 
     void Start()
     {
         character = GetComponent<Character>();
-
     }
 
     void OnEnable()
     {
-        inputSpell1.action.performed += CastAttack;
-        inputSpell2.action.performed += CastAttack;
         inputSpell1.action.Enable();
         inputSpell2.action.Enable();
+        inputSpell3.action.Enable();
+        inputSpell4.action.Enable();
     }
-
 
     void OnDisable()
     {
-        inputSpell1.action.performed -= CastAttack;
-        inputSpell2.action.performed -= CastAttack;
         inputSpell1.action.Disable();
         inputSpell2.action.Disable();
+        inputSpell3.action.Disable();
+        inputSpell4.action.Disable();
     }
 
-    private void CastAttack(InputAction.CallbackContext context) {
-        int indexSpell = 0;
-        switch (context.action.name)
-        {
-            case "QSpell":
-                indexSpell = 0;
-                break;
-            case "ZSpell":
-                indexSpell = 1;
-                break;
-            case "ESpell":
-                indexSpell = 2;
-                break;
-            case "RSpell":
-                indexSpell = 3;
-                break;
-        }
+    void Update()
+    {
+        // Vérifie si la touche est maintenue
+        if (inputSpell1.action.IsPressed())
+            CastAttack(0);
+
+        if (inputSpell2.action.IsPressed())
+            CastAttack(1);
+
+        if (inputSpell3.action.IsPressed())
+            CastAttack(2);
+
+        if (inputSpell4.action.IsPressed())
+            CastAttack(3);
+    }
+
+    private void CastAttack(int indexSpell)
+    {
         if (character.spells.Length > indexSpell)
         {
-            character.spells[indexSpell].AttackWithCooldown(transform.position, transform.forward, enemyLayer);
+            character.spells[indexSpell].AttackWithCooldown(
+                transform.position,
+                transform.forward,
+                enemyLayer
+            );
         }
         else
         {
             Debug.LogError("No spell for this input");
         }
-
     }
 }
